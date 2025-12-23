bb20ceb5c373ea1b0c49c75dca66bd86542ff472
Mahfuzur Rahman <sm.rahman5566@gmail.com>
2025-12-23 17:46:28 -0500

    Day 2 - Part 1 - Cleaned up and wrote functionally

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,49 +1,35 @@
 use std::fs::read_to_string;
 
 fn main() {
+    part_one();
+}
+
+fn part_one() {
     let file_path = "src/data.txt";
     let contents = read_to_string(file_path)
     .expect("Should have read data.txt");
 
-    let mut result: u64 = 0;
+    let result: u64 = contents
+    .lines()
+    .flat_map(|line| line.split(","))
+    .filter_map(|range| {
+        let (start, end) = range.split_once('-')?;
+        Some(repeated_twice(start, end))
+    })
+    .sum();
 
-    for line in contents.lines() {
-        let ranges = line.split(",");
-        for range in ranges {
-            let mut range_iter = range.split("-");
-            let start = range_iter.next().unwrap();
-            let end = range_iter.next().unwrap();
-            let repeated_twice_sum = repeated_twice(start, end);
-            result += repeated_twice_sum;
-                
-        }
-    }
-    println!("Final Result: {result}");
+    println!("{result}");
 }
 
 fn repeated_twice(start: &str, end: &str) -> u64 {
-    let mut result: u64 = 0;
-    let start_int: u64 = start.parse().unwrap();
-    let end_int: u64 = end.parse().unwrap();
+    let start: u64 = start.parse().expect("Invalid number");
+    let end: u64 = end.parse().expect("Invalid number");
 
-    for number in start_int..=end_int {
-        let number_str = number.to_string();
-        let length: usize = number_str.len();
-        if length % 2 == 0 {
-            let mut first_half = String::from("");
-            for i in 0..=length/2-1 {
-                first_half.push(number_str.chars().nth(i).unwrap());
-            } 
-            let mut second_half = String::from("");
-            for i in length/2..=length-1 {
-                second_half.push(number_str.chars().nth(i).unwrap());
-            } 
-
-
-            if first_half.eq(&second_half) {
-                result += number;
-            }
-        }
-    }
-    return result;
+    (start..=end)
+    .filter(|&number| {
+        let s = number.to_string();
+        let mid = s.len() / 2;
+        s.len() % 2 == 0 && s[..mid] == s[mid..]
+    })
+    .sum()
 }
